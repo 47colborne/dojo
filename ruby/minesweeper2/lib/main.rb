226c3425c45e9db308d3b8eb377affc417845cb7
David <david@retailcommon.com>
2015-03-27 17:08:42 -0400

    Implemented click and reveal events

--- ruby/minesweeper2/lib/main.rb
+++ ruby/minesweeper2/lib/main.rb
@@ -1,6 +1,7 @@
 require 'ostruct'
 
 Location = Struct.new(:row, :column)
+Event = Struct.new(:type, :location)
 
 class RandomPlacementStrategy
   def mine_locations(num_mines, rows, columns)
@@ -43,7 +44,7 @@ class Minefield
     end
   end
 
-  def to_s_m
+  def display
     @field.collect { |row| row.collect { |cell| cell.to_s } }
   end
 
@@ -54,26 +55,37 @@ class Minefield
   def cells
     @field.flatten
   end
+
+  def guess(row,column)
+    cell = @field[row][column]
+    cell.click(cell.location)
+  end
 end
 
 class Cell
   MINE = -1
+  MASK = "#"
 
-  attr_reader :location
+  attr_reader :location, :value, :hidden
 
   def initialize(field, location)
     @value = 0
     @field = field
     @location = location
+    @hidden = true
   end
 
   def to_s
-    (@value == MINE) ? '*' : @value.to_s
+    if hidden
+      MASK
+    else
+      (has_mine?) ? '*' : @value.to_s
+    end
   end
 
   def insert_mine
     @value = MINE
-    @field.dispatch(OpenStruct.new(type: :mine_placed, location: location))
+    @field.dispatch(Event.new(:mine_placed, location))
   end
 
   def handle(event)
@@ -84,14 +96,45 @@ class Cell
     increment_value if neighbour?(location)
   end
 
+  def has_mine?
+    @value == MINE
+  end
+
+  def reveal(location=nil)
+    @hidden = false
+  end
+
+  def click(location)
+    if @location == location
+
+      if blank?
+        @field.dispatch(Event.new(:click, @location)) if hidden
+      end
+      @field.dispatch(Event.new(:reveal, @location)) if has_mine?
+      reveal
+
+    elsif neighbour?(location)
+      if blank?
+        @field.dispatch(Event.new(:click, @location)) if hidden
+        reveal
+      end
+    end
+  end
+
+  def increment_value
+    @value += 1 unless has_mine?
+  end
+
+  def blank?
+    @value == 0
+  end
+
   private
 
   def neighbour?(other_location)
     (location.column - other_location.column).abs <= 1 &&
-        (location.row - other_location.row).abs <= 1
+    (location.row - other_location.row).abs <= 1
   end
 
-  def increment_value
-    @value += 1 unless @value == MINE
-  end
+
 end
--- ruby/minesweeper2/spec/main_spec.rb
+++ ruby/minesweeper2/spec/main_spec.rb
@@ -25,16 +25,18 @@ describe Minefield do
   let(:minefield) { Minefield.new(placement_strategy, 2, 3) }
 
   it 'should start with an empty field' do
-    expect(minefield.to_s_m).to eq([%w(0 0 0),
-                                    %w(0 0 0)])
+    minefield.dispatch(Event.new(:reveal, nil))
+    expect(minefield.display).to eq([%w(0 0 0),
+                                     %w(0 0 0)])
   end
 
   context 'mine laying' do
     it 'should increment all adjacent cells value' do
+      minefield.dispatch(Event.new(:reveal, nil))
       expect(placement_strategy).to receive(:mine_locations).and_return([Location.new(0, 0)])
       minefield.lay_mines(1)
-      expect(minefield.to_s_m).to eq([%w(* 1 0),
-                                      %w(1 1 0)])
+      expect(minefield.display).to eq([%w(* 1 0),
+                                       %w(1 1 0)])
     end
   end
 
@@ -47,6 +49,173 @@ describe Minefield do
       end
 
       minefield.dispatch(event)
+    end
+  end
+
+  describe '#guess' do
+    it 'should click the guessed cell' do
+      location = Location.new(0,0)
+      expect(minefield.cells.first).to receive(:click).with(location)
+      minefield.guess(0,0)
+    end
+  end
+end
+
+describe Cell do
+
+  let(:field) { instance_double(Minefield, :dispatch => {}) }
+  let(:location) { Location.new(1, 1) }
+
+  subject(:cell) { Cell.new(field, location) }
+
+  describe '#insert_mine' do
+    it 'should insert a mine' do
+      cell.insert_mine
+      expect(cell).to have_mine
+    end
+
+    it 'should notify minefield' do
+      expect(field).to receive(:dispatch).with(Event.new(:mine_placed, location))
+      cell.insert_mine
+    end
+  end
+
+  context 'upon creation' do
+    it 'should be hidden' do
+      expect(cell.hidden).to eq(true)
+    end
+  end
+
+  describe '#to_s' do
+    it 'should not display its value if hidden' do
+      expect(cell.to_s).to eq(Cell::MASK)
+    end
+
+    it 'should return its value if not hidden' do
+      cell.reveal
+      expect(cell.to_s).to eq('0')
+
+    end
+  end
+
+  describe '#reveal' do
+    it 'should set hidden to false' do
+      cell.reveal
+      expect(cell.hidden).to eq(false)
+    end
+  end
+
+  describe '#click' do
+
+    context 'on numbered cell' do
+      before do
+        cell.increment_value
+        expect(field).to_not receive(:dispatch)
+      end
+
+      context 'with this cell`s location' do
+        it 'should reveal the cell' do
+          cell.click(location)
+          expect(cell.hidden).to eq(false)
+        end
+      end
+
+      context 'with a neighbouring location' do
+        it 'should remain hidden' do
+          cell.click(Location.new(location.row + 1, location.column))
+          expect(cell.hidden).to eq(true)
+        end
+      end
+
+      context 'with a non-neighbouring location' do
+        it 'should remain hidden' do
+          cell.click(Location.new(location.row + 2, location.column + 1))
+          expect(cell.hidden).to eq(true)
+        end
+      end
+    end
+
+    context 'blank cell' do
+      context 'with this cell\'s location' do
+        it 'should reveal this cell and propagate' do
+          expect(field).to receive(:dispatch).with(Event.new(:click, location))
+          cell.click(location)
+          expect(cell.hidden).to eq(false)
+        end
+      end
+
+      context 'with a neighbouring location' do
+        it 'should reveal this cell and propagate' do
+          expect(field).to receive(:dispatch).with(Event.new(:click, location))
+          cell.click(Location.new(location.row+1, location.column+1))
+          expect(cell.hidden).to eq(false)
+        end
+      end
+
+      context 'with a non-neighbouring location' do
+        it 'should remain hidden' do
+          expect(field).not_to receive(:dispatch)
+          cell.click(Location.new(location.row+2, location.column+2))
+          expect(cell.hidden).to eq(true)
+        end
+      end
+    end
+
+    context 'mine cell' do
+      before do
+        cell.insert_mine
+      end
+
+      context 'with this cell`s location' do
+        it 'should reveal the cell' do
+          expect(field).to receive(:dispatch).with(Event.new(:reveal, location))
+          cell.click(location)
+          expect(cell.hidden).to eq(false)
+        end
+      end
+
+      context 'with a neighbouring location' do
+        it 'should remain hidden' do
+          expect(field).to_not receive(:dispatch)
+          cell.click(Location.new(location.row + 1, location.column))
+          expect(cell.hidden).to eq(true)
+        end
+      end
+
+      context 'with a non-neighbouring location' do
+        it 'should remain hidden' do
+          expect(field).to_not receive(:dispatch)
+          cell.click(Location.new(location.row + 2, location.column + 1))
+          expect(cell.hidden).to eq(true)
+        end
+      end
+    end
+  end
+
+  describe '#mine_placed' do
+    it 'should increment value if a mine is placed next to it' do
+      expect { cell.mine_placed(Location.new(0, 0)) }.to change { cell.value }.by(1)
+    end
+
+    it 'should not increment value if a mine is placed far from it' do
+      expect { cell.mine_placed(Location.new(2, 3)) }.not_to change { cell.value }
+    end
+
+    it 'should not change value if the cell already contains a mine' do
+      cell.insert_mine
+      expect { cell.mine_placed(Location.new(1, 0)) }.not_to change { cell.value }
+    end
+  end
+
+  describe '#handle' do
+    it 'should invoke method based on given event type' do
+      expect(cell).to receive(:respond_to?).with(:foo).and_return(true)
+      expect(cell).to receive(:send).with(:foo, location)
+      cell.handle(Event.new(:foo, location))
+    end
+    it 'should not invoke a method that it does not respond to' do
+      expect(cell).to_not receive(:send)
+      cell.handle(Event.new(:foo, location))
     end
   end
 end
