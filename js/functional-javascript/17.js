0e410d1194ced8ed3bb36b0585b235ecabd5825f
David <david@retailcommon.com>
2015-06-30 14:59:03 -0400

    Completed Functional JavaScript

--- js/functional-javascript/17.js
+++ js/functional-javascript/17.js
@@ -1,19 +1,19 @@
+'use strict';
+
 function curryN(fn, n, params) {
-    if (arguments.length < 3)
-        params = [];
+    n = n || fn.length;
+    params = params || [];
 
-    return function (baseFunction) {
-        return function (val) { // curry function
-            if (params.length < baseFunction.length)
-                params.push(val);
+    return function () { // curry function
+        var combinedParams = params.concat(Array.prototype.slice.call(arguments));
 
-            if (params.length == baseFunction.length)
-                return baseFunction.apply(this, params);
-            else {
-                return curryN(baseFunction, 0, params);
-            }
+        if (combinedParams.length >= n) {
+            return fn.apply(this, combinedParams);
         }
-    }(fn, params);
+        else {
+            return curryN(fn, n, combinedParams);
+        }
+    };
 }
 
 module.exports = curryN;
