b60950a40f0a1e2f0fec8ea47869faef3d6e058f
makeready <shotsinthedark@gmail.com>
2015-09-11 10:52:08 -0400

    update to openeststruct

--- ruby/openest_struct/lib/opener_struct.rb
+++ ruby/openest_struct/lib/opener_struct.rb
@@ -4,42 +4,44 @@ class OpenestStruct < Object
 
   def initialize(args = {})
     @data = args
-    initialize_methods
+    __initialize_methods
   end
 
   def method_missing(symbol, *args, &block)
-    if is_setter?(symbol)
+    if __is_setter?(symbol)
       getter = symbol.to_s[0..-2].to_sym
-      define_getter(getter)
-      define_setter(symbol, args[0])
+      @data[getter] = args[0]
+      __define_getter(getter, args[0])
+      __define_setter(symbol)
     end
   end
 
-  def is_setter? symbol
+  def __is_setter? symbol
     symbol.to_s[-1] == "="
   end
 
   private
 
-  def initialize_methods
+  def __initialize_methods
     @data.each do |key, value|
-
-      define_getter(key)
-
+      __define_getter(key, value)
       setter_name = key.to_s + '='
-      define_setter(setter_name)
+      __define_setter(setter_name)
 
     end
   end
 
-  def define_getter(symbol)
-    define_singleton_method(symbol) { @data[symbol] }
+  def __define_getter(symbol, value)
+    block = value.is_a?(Proc) ? value : -> { @data[symbol] }
+    define_singleton_method(symbol, &block)
   end
 
-  def define_setter(symbol, default_value = nil)
+  def __define_setter(symbol)
     key = symbol[0..-2].to_sym
-    define_singleton_method(symbol) { |value| @data[key] = value }
-    @data[key] = default_value if default_value
+    define_singleton_method(symbol) do |value|
+      __define_getter(key, value)
+      @data[key] = value
+    end
   end
 
 end
--- ruby/openest_struct/spec/opener_struct_spec.rb
+++ ruby/openest_struct/spec/opener_struct_spec.rb
@@ -11,42 +11,67 @@ describe OpenestStruct do
     end
   end
 
-  it 'should initalize a method with proc passed in' do
+  describe '#define_getter' do
+    it 'should initalize a method with proc passed in' do
       model.sum = -> (value1, value2) { value1 + value2 }
       expect(model.sum(1, 10)).to eq(11)
-  end
+    end
 
-  it 'should be able to write to any attribute' do
-    model.title = "Yroo Struct"
-    expect(model.title).to eq("Yroo Struct")
-  end
+    it 'should re-defined the attribute getter' do
+      subject.sum = 47
+      subject.sum = -> (value1, value2) { value1 + value2 }
+      expect(subject.sum(1, 9)).to eq(10)
+      subject.sum = 47
+      expect(subject.sum).to eq(47)
+    end
+    it 'should never raise method_missing for a getter' do
+      expect{model.title}.to_not raise_error
+    end
 
-  it 'should never raise method_missing for a getter' do
-    expect{model.title}.to_not raise_error
-  end
+    it 'should return nil when attribute is not set' do
+      expect(model.title).to be nil
+    end
 
-  it 'should return nil when attribute is not set' do
-    expect(model.title).to be nil
-  end
-
-  it 'should define the reader as an explicit method' do
-    model.title = "Yroo Struct"
-    expect(model.methods(false)).to include(:title)
-  end
-
-  it 'should define the writer explicitly' do
-    model.title = "Yroo Struct"
-    expect(model.methods(false)).to include(:title=)
+    it 'should define the reader as an explicit method' do
+      model.title = "Yroo Struct"
+      expect(model.methods(false)).to include(:title)
+    end
   end
 
   describe '#is_setter?' do
+    it 'should define the writer explicitly' do
+      model.title = "Yroo Struct"
+      expect(model.methods(false)).to include(:title=)
+    end
+    it 'should be able to write to any attribute' do
+      model.title = "Yroo Struct"
+      expect(model.title).to eq("Yroo Struct")
+    end
     it 'should be able to tell if the missing method is a setter' do
-      expect(model.is_setter?(:title=)).to be_truthy
+      expect(model.__is_setter?(:title=)).to be_truthy
     end
 
     it 'should be able to tell if the missing method is not a setter' do
-      expect(model.is_setter?(:title)).to be_falsey
+      expect(model.__is_setter?(:title)).to be_falsey
     end
   end
 
+  it 'should be able to access other attribute' do
+    subject.first_name = "Scott"
+    subject.last_name = "Chu"
+    subject.full_name = -> { first_name + " " + last_name }
+    expect(subject.full_name).to eq('Scott Chu')
+  end
+
+  it 'should be able to call other methods' do
+    subject.sum = -> (value1, value2) { value1 + value2 }
+    subject.sum_plus_one = -> (value1, value2) {sum(value1, value2) + 1 }
+    expect(subject.sum_plus_one(1,1)).to eq(3)
+  end
+
+  it 'should allow the setting of any attribute name' do
+    subject.define_setter = 1
+    expect(subject.define_setter).to eq(1)
+  end
 end
+
