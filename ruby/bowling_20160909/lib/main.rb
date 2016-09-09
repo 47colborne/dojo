56e5f06572d9828ef3fc507bc3fcbf6b09ca3dcb
David <david@retailcommon.com>
2016-09-09 18:49:53 -0400

    completed bowling kata

--- ruby/bowling_20160909/lib/main.rb
+++ ruby/bowling_20160909/lib/main.rb
@@ -1,17 +1,35 @@
 class Game
   def self.score(frame_data)
     frames = Frames.new(frame_data)
-    frames.inject(0) { |sum, e| sum + e.score }
+    frames.score
   end
 end
 
 class Frames
   def initialize(frame_data)
-    # TODO
+    @head = EmptyFrame.new
+
+    frame_data.reverse.each do |rolls|
+      frame = Frame.from_rolls(rolls, @head)
+      @head = frame
+    end
+  end
+
+  def score
+    do_score(@head)
+  end
+
+  private
+
+  def do_score(frame)
+    return 0 if frame.nil?
+    frame.score + do_score(frame.next_frame)
   end
 end
 
 class Frame
+  attr_reader :next_frame
+
   def initialize(rolls, next_frame = EmptyFrame.new)
     @rolls = rolls
     @next_frame = next_frame
@@ -29,31 +47,39 @@ class Frame
     @rolls.reduce(:+)
   end
 
-  def self.from_rolls(rolls)
-    return StrikeFrame.new(rolls) if rolls[0] == 10
-    return SpareFrame.new(rolls) if rolls[0] + rolls[1] == 10
-    new(rolls)
+  def self.from_rolls(rolls, next_frame = EmptyFrame.new)
+    if rolls[0] == 10
+      StrikeFrame.new(rolls, next_frame)
+    elsif rolls[0] + rolls[1] == 10
+      SpareFrame.new(rolls, next_frame)
+    else
+      new(rolls, next_frame)
+    end
   end
 end
 
 class SpareFrame < Frame
   def score
-    super + @next_frame.first_roll
+    super + next_frame.first_roll
   end
 end
 
 class StrikeFrame < Frame
   def score
-    super + @next_frame.first_roll + @next_frame.second_roll
+    super + next_frame.first_roll + next_frame.second_roll
   end
 
   def second_roll
-    @next_frame.first_roll
+    @rolls[1] || next_frame.first_roll
   end
 end
 
 class EmptyFrame < Frame
   def initialize
+  end
+
+  def score
+    0
   end
 
   def first_roll
--- ruby/bowling_20160909/spec/main_spec.rb
+++ ruby/bowling_20160909/spec/main_spec.rb
@@ -4,61 +4,65 @@ describe Game do
   describe '.score' do
     context 'when scoring a single frame' do
       it 'gets the score from the frame' do
-        score = Game.score([[2,5]])
+        score = Game.score([[2, 5]])
         expect(score).to eq(7)
       end
     end
+
     context 'when scoring multiple regular frames' do
       it 'sums the score of each frame' do
-        score = Game.score([[1,2],[3,4]])
+        score = Game.score([[1, 2], [3, 4]])
         expect(score).to eq(10)
       end
     end
+
     context 'when scoring a spare' do
       context 'that is the final frame' do
         it 'should get the score from the frame with the third roll as the bonus' do
-          score = Game.score([[3,7,2]])
+          score = Game.score([[3, 7, 2]])
           expect(score).to eq(12)
         end
       end
+
       context 'that is not the last frame' do
         it 'should add the first roll of the next frame to the spare frame' do
-          score = Game.score([[2,8],[3,2]])
+          score = Game.score([[2, 8], [3, 2]])
           expect(score).to eq(18)
         end
       end
+    end
+
+    it 'scores a perfect game' do
+      score = Game.score([[10], [10], [10], [10], [10], [10], [10], [10], [10], [10, 10, 10]])
+      expect(score).to eq(300)
     end
   end
 end
 
 describe Frame do
-  describe '#score' do
-    let(:frame) { Frame.new([1, 2]) }
+  let(:frame) { Frame.new([1, 2]) }
 
-    it 'returns the sum of the pins knocked down' do
+  describe '#score' do
+    it 'returns the sum it`s rolls' do
       expect(frame.score).to eq(3)
     end
   end
 
   describe '#first_roll' do
-    let(:frame) { Frame.new([3, 4]) }
-
     it 'returns the first of the frame\'s rolls' do
-      expect(frame.first_roll).to eq(3)
+      expect(frame.first_roll).to eq(1)
     end
   end
 
   describe '#second_roll' do
-    let(:frame) { Frame.new([3, 4]) }
-
     it 'returns the second of the frame\'s rolls' do
-      expect(frame.second_roll).to eq(4)
+      expect(frame.second_roll).to eq(2)
     end
   end
 
   describe '.from_rolls' do
     it 'converts a set of rolls into a frame' do
-      frame = Frame.from_rolls([2,4])
+      frame = Frame.from_rolls([2, 4])
       expect(frame).to be_an_instance_of(Frame)
     end
 
@@ -71,7 +75,7 @@ describe Frame do
 
     context 'when the first and second rolls sum to ten' do
       it 'should create a SpareFrame' do
-        frame = Frame.from_rolls([2,8])
+        frame = Frame.from_rolls([2, 8])
         expect(frame).to be_an_instance_of(SpareFrame)
       end
     end
@@ -80,36 +84,68 @@ end
 
 describe SpareFrame do
   describe '#score' do
-    let(:frame) { SpareFrame.new([5, 5], Frame.new([2, 1])) }
-    it 'should return the sum of rolls plus the first roll' do
-      expect(frame.score).to eq(12)
+    let(:frame) { SpareFrame.new([5, 5], next_frame) }
+    let(:next_frame) { instance_double(Frame, first_roll: 3, second_roll: 4) }
+
+    it 'includes the first roll of the next frame as a bonus' do
+      expect(frame.score).to eq(13)
     end
   end
 end
 
-
 describe StrikeFrame do
+  let(:frame) { StrikeFrame.new([10], next_frame) }
+  let(:next_frame) { instance_double(Frame, first_roll: 3, second_roll: 4) }
+
   describe '#score' do
-    let(:frame) { StrikeFrame.new([10], Frame.new([7, 1])) }
-    it 'should return the sum of rolls plus the next two rolls' do
-      expect(frame.score).to eq(18)
+    it 'includes the next two rolls as a bonus' do
+      expect(frame.score).to eq(17)
     end
   end
 
   describe '#second_roll' do
-    let(:frame) { StrikeFrame.new([10], next_frame) }
-    let(:next_frame) { Frame.new([5, 3]) }
+    context 'in a regular strike frame' do
+      it 'returns the first roll from the next frame' do
+        expect(frame.second_roll).to eq(3)
+      end
 
-    it 'returns the second of the frame\'s rolls' do
-      expect(frame.second_roll).to eq(5)
+      context 'that has no next_frame' do
+        let(:frame) { StrikeFrame.new([10]) }
+
+        it 'returns 0 as its second roll' do
+          expect(frame.second_roll).to eq(0)
+        end
+      end
     end
 
-    context 'has no next_frame' do
-      let(:frame) { StrikeFrame.new([10]) }
+    context 'in the final frame' do
+      let(:frame) { StrikeFrame.new([10, 1, 2]) }
 
-      it 'returns 0 as its second roll' do
-        expect(frame.second_roll).to eq(0)
+      it 'returns its second roll' do
+        expect(frame.second_roll).to eq(1)
       end
+    end
+  end
+end
+
+describe EmptyFrame do
+  let(:frame) { EmptyFrame.new }
+
+  describe '#score' do
+    it 'returns 0' do
+      expect(frame.score).to eq(0)
+    end
+  end
+
+  describe '#first_roll' do
+    it 'returns 0' do
+      expect(frame.first_roll).to eq(0)
+    end
+  end
+
+  describe '#second_roll' do
+    it 'returns 0' do
+      expect(frame.second_roll).to eq(0)
     end
   end
 end
